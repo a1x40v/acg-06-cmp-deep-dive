b1631cc826e3e220670154507388f69bcfdfd33e
a1x40v <a1x40v@gmail.com>
2024-08-06 20:06:04 +0300

    Split app to multiple components

---
+++ src/app/dashboard/server-tickets/server-tickets.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-tickets.component.html',
+  styleUrl: './server-tickets.component.css',
+})
+export class ServerTicketsComponent {}
