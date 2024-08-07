f082030d3012a4f93e09cb00418cc62ce0f1513b
a1x40v <a1x40v@gmail.com>
2024-08-07 16:40:04 +0300

    Template variables and ViewChild

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  viewChild,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [FormsModule, ButtonComponent, ControlComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  onSubmit(title: string, ticketText: string) {
+    this.form().nativeElement.reset();
+  }
+}
