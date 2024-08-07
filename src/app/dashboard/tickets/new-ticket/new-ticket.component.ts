3b3af7ffe133f68df75c057b6edfd98d27a1477c
a1x40v <a1x40v@gmail.com>
2024-08-07 18:52:57 +0300

    Create and show tickets

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -2,6 +2,7 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  output,
   viewChild,
   ViewChild,
   ViewChildren,
@@ -21,13 +22,15 @@ import { ControlComponent } from '../../../shared/control/control.component';
 export class NewTicketComponent implements AfterViewInit {
   // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  add = output<{ title: string; text: string }>();
 
   ngAfterViewInit() {
     console.log('after view init');
     console.log(this.form().nativeElement);
   }
 
-  onSubmit(title: string, ticketText: string) {
+  onSubmit(title: string, text: string) {
+    this.add.emit({ title, text });
     this.form().nativeElement.reset();
   }
 }
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { Ticket } from './ticket.model';
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
-export class TicketsComponent {}
+export class TicketsComponent {
+  tickets: Ticket[] = [];
+
+  onAdd(data: { title: string; text: string }) {
+    const ticket: Ticket = {
+      id: Math.random().toString(),
+      title: data.title,
+      request: data.text,
+      status: 'open',
+    };
+
+    this.tickets.push(ticket);
+  }
+}
